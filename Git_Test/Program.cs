17ce03a12cf4813555dceedbe805f7be6ab065a8
UOEUASNNTE <aoeuhaso@gmail.com>
2021-12-05 15:28:41 +0300

    $next

--- Git_Test/Program.cs
+++ Git_Test/Program.cs
@@ -10,7 +10,7 @@ namespace Git_Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Everyone!");
         }
     }
 }
